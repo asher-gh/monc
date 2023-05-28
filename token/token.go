0350d95e56d637c497bab01b48cf83c3d05052da
Asher <ashish.2017@hotmail.com>
2023-05-28 11:04:11 +0100

    keywords, identifiers and ints

--- token/token.go
+++ token/token.go
@@ -3,32 +3,44 @@ package token
 type TokenType string
 
 type Token struct {
-   Type TokenType
-   Literal string
+	Type    TokenType
+	Literal string
 }
 
 const (
-   ILLEGAL = "ILLEGAL"
-   EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-   // Identifiers + literals
-   IDENT = "IDENT"
-   INT = "INT"
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT   = "INT"
 
-   // Operators
-   ASSIGN = "="
-   PLUS = "+"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
 
-   // Delimiters
-   COMMA = ","
-   SEMICOLON = ";"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-   LPAREN = "("
-   RPAREN = ")"
-   LBRACE = "{"
-   RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-   // Keywords
-   FUNCTION = "FUNCTION"
-   LET = "LET"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+   if tok, ok := keywords[ident]; ok {
+      return tok
+   }
+   return IDENT
+}
