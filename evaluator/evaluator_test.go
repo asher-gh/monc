42926da996f8622d0d42a21210f08bb6f4c64c21
Asher <ashish.2017@hotmail.com>
2023-06-25 20:09:14 +0100

    evaluating arrray literals and index operators

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -34,7 +34,6 @@ func testIntegerObject(t *testing.T, obj object.Object, i int64) bool {
 	}
 
 	if res.Value != i {
-
 		t.Errorf("object has wrong value. got=%d, want=%d", res.Value, i)
 		return false
 	}
@@ -436,6 +435,53 @@ func TestBuiltinfunctions(t *testing.T) {
 			if errObj.Message != expected {
 				t.Errorf("wrong error message. expected=%q, got=%q", expected, errObj.Message)
 			}
+		}
+	}
+}
+
+// ==================================== ARRAYS ====================================
+func TestArrayLiterals(t *testing.T) {
+	input := "[1, 2 * 2, 3 + 3]"
+
+	evaluated := testEval(input)
+	res, ok := evaluated.(*object.Array)
+
+	if !ok {
+		t.Fatalf("object is not Array. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if ln := len(res.Elements); ln != 3 {
+		t.Fatalf("array has wrong num of elements. got=%d", ln)
+	}
+
+	testIntegerObject(t, res.Elements[0], 1)
+	testIntegerObject(t, res.Elements[1], 4)
+	testIntegerObject(t, res.Elements[2], 6)
+}
+
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"[1, 2, 3][0]", 1},
+		{"[1, 2, 3][1]", 2},
+		{"[1, 2, 3][2]", 3},
+		{"let i = 0; [1][i]", 1},
+		{"[1, 2, 3][1 + 1]", 3},
+		{"let myArray = [1, 2, 3]; myArray[2];", 3},
+		{"let myArray = [1, 2, 3]; let i = myArray[0]; myArray[i];", 2},
+		{"let myArray = [1, 2, 3]; myArray[0] + myArray[1] + myArray[2]", 6},
+		{"[1,2,3][3]", nil},
+		{"[1,2,3][-1]", nil},
+	}
+
+	for _, tt := range tests {
+		evaled := testEval(tt.input)
+		if integer, ok := tt.expected.(int); ok {
+			testIntegerObject(t, evaled, int64(integer))
+		} else {
+			testNullObject(t, evaled)
 		}
 	}
 }
--- object/object.go
+++ object/object.go
@@ -19,6 +19,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARARY"
 )
 
 type Builtin struct {
@@ -94,6 +95,26 @@ func (f *Function) Inspect() string {
 	out.WriteString(") {\n")
 	out.WriteString(f.Body.String())
 	out.WriteString("\n}")
+
+	return out.String()
+}
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
 
 	return out.String()
 }
