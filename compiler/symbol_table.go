e4594c0db67c6f1da09c59bc787f6cbb3792bf41
Asher <ashish.2017@hotmail.com>
2023-07-26 20:25:51 +0100

    feat: symbol table

---
+++ compiler/symbol_table.go
@@ -0,0 +1,23 @@
+package compiler
+
+type SymbolScope string
+
+const (
+	GlobalScope string = "GLOBAL"
+)
+
+type Symbol struct {
+	Name  string
+	Scope SymbolScope
+	Index int
+}
+
+type SymbolTable struct {
+	store          map[string]Symbol
+	numDefinitions int
+}
+
+func NewSymbolTable() *SymbolTable {
+	s := make(map[string]Symbol)
+	return &SymbolTable{store: s}
+}
